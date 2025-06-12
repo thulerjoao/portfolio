8d3eb26df356c9667294a0a212de6ee21f084d17
thulerjoao <thuler_lima@hotmail.com>
2025-06-11 22:03:30 -0300

    updated autoplay in videos

--- src/Components/Projects/components/dataRain/index.tsx
+++ src/Components/Projects/components/dataRain/index.tsx
@@ -90,7 +90,7 @@ const Datarain = ({ language }: Props) => {
             <h4>Application overview:</h4>
           )}
           <div className="video front native">
-            <video controls autoPlay muted loop>
+            <video controls autoPlay playsInline muted loop>
               <source src={DataRainVideo} type="video/mp4" />
               Seu navegador não suporta o elemento de vídeo.
             </video>
